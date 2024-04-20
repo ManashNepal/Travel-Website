bf3de9f8e0635bf1478374966627e738b1626c5a
ManashNepal <manash.nepal111@gmail.com>
2024-04-20 13:18:20 +0530

    minor fixes

--- script.js
+++ script.js
@@ -92,6 +92,14 @@ iconElements.forEach(iconElement=>{
     });
 });
 
+// Toggle
+let menu = document.querySelector('#menu-icon');
+let navmenu = document.querySelector('.navmenu');
+
+menu.onclick = ()=>{
+    menu.classList.toggle('bx-x');
+    navmenu.classList.toggle('open');
+}
 
 
 
